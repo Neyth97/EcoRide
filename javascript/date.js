0a4a993c031170c528f416aff13456bf9813d92b
Neyth97 <loic.rovelas@gmail.com>
2025-03-26 14:09:23 +0100

    Ajout d'une icone calendrier cliquable, ouvrant également le calendrier de réservation

--- javascript/date.js
+++ javascript/date.js
@@ -1,3 +1,19 @@
+// Ajout de l'outil de sélection de date
+// Utilisation de la librairie flatpickr
+
 flatpickr("#datepicker", {
     dateFormat: "d/m/Y"
+  });
+
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // Sélectionne l'icône de calendrier et le champ de date
+    const calendarIcon = document.querySelector(".input-group-text");
+    const dateInput = document.querySelector("#datepicker");
+  
+    // Ajoute un événement "click" sur l'icône
+    calendarIcon.addEventListener("click", function () {
+      dateInput.focus(); // Simule un clic dans le champ de date
+    });
   });
